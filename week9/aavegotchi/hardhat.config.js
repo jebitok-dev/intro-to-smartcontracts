d610004a442fc975cd0d3ab5929a09bb8b2b46b0
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-03-09 23:35:12 +0300

    Add NFT Creating, Compile and Deploy

---
+++ week9/aavegotchi/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const {ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY} = process.env;
+
+module.exports = {
+  solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`],
+    },
+    // ethereum: {
+    //   chainId: 1,
+    //   url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+    //   accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    // },
+  },
+};

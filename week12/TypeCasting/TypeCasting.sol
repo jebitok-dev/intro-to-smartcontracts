be7a5205e46fa913bc83c366f185215e4d5193e8
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-03-29 23:48:27 +0300

    Add Basic on TypeCasting

---
+++ week12/TypeCasting/TypeCasting.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract TypeCast {
+// you can pass any data on abi.encode
+// you can't encode struct
+// constructing domain operators use abi.encodePacked
+string(abi.encodePacked("temi", "tayo"))
+string(abi.encodePacked("temi", "tayo), ",")
+// append => appendInt
+mapping(uint => mapping(uint => Names) noToName); 
+// import file means you have access to contract
+
+// Diamond standard optimization...remove unused code
+
+}
---
+++ week12/diamond.sol
@@ -0,0 +1,5 @@
+// Diamond standard
+// connected project and realized : fork project/clone
+// diamond standard vs splitting
+// reduce byte code increase optimization runs: optimise and introduce byte code
+// founder of openzeppelin: helped someone
